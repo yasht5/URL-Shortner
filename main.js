dae1c13effcaff46bcc6aeef981604b53ff1f7f1
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2021-06-04 23:33:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+// Information to reach API
+const apiKey = '12017b79d05e402ab61400bfc2c4d0a1';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = () =>{
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      //renderRawResponse(xhr.response);
+      renderResponse(xhr.response);
+    }
+  }
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.setRequestHeader('apikey', apiKey);
+  xhr.send(data);
+}
+
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
